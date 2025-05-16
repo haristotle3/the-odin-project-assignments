64006d11713991bf1a787fff631b0698f50628d0
haristotle <haristotlepatil2003@gmail.com>
2025-05-16 23:55:52 +0530

    Wrote getHumanChoice

--- Foundations/rock-paper-scissor-console/script.js
+++ Foundations/rock-paper-scissor-console/script.js
@@ -4,3 +4,24 @@ function getComputerChoice() {
 
   return choices[choice];
 }
+
+function getHumanChoice() {
+  let choice = prompt("Rock or Paper or Scissor ?", "rock");
+  return choice.toLowerCase();
+}
+
+function playRound() {
+  let humanChoice = getHumanChoice();
+  let computerChoice = getComputerChoice();
+
+  switch (humanChoice) {
+    case "rock":
+      if (computerChoice === "rock") console.log("");
+      else if (computerChoice === "paper") console.log("");
+      else if (computerChoice === "scissor") console.log("");
+      break;
+  }
+}
+
+let humanScore = 0,
+  computerScore = 0;
