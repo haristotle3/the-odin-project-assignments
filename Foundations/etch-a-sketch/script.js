f616adce8dad6678e206399e0aa36876a40c2bb1
haristotle <haristotlepatil2003@gmail.com>
2025-05-21 12:18:04 +0530

    initGrid implemented. Can initalize any n x n grid.

---
+++ Foundations/etch-a-sketch/script.js
@@ -0,0 +1,30 @@
+const CONTAINER_WIDTH = 960;
+
+function initGrid(gridSize = 16) {
+    if(gridSize > 100) {
+        alert("Please enter grid size less than 100.");
+        return;
+    }
+
+    const container = document.querySelector(".container");
+
+    // Remove all existing divs.
+    while(container.lastElementChild) {
+        container.lastElementChild.remove();
+    }
+
+    // create n x n grid. Given gridSize
+    const divSize = CONTAINER_WIDTH / gridSize;
+    for(let i = 0; i < gridSize; i++) {
+        for(let j = 0; j < gridSize; j++) {
+            const box = document.createElement("div");
+            box.style.boxSizing = "border-box"
+            box.style.width = box.style.height = divSize + "px"; 
+            box.style.border = "1px solid rgb(192, 192, 192)"
+            box.style.margin = box.style.padding = 0;
+            container.appendChild(box);
+        }
+    }
+}
+
+initGrid(64)
