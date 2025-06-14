0db79f2c667e63027215462f25ac1358658fad11
haristotle <haristotlepatil2003@gmail.com>
2025-06-14 19:58:18 +0530

    Test addBookToLibrary and refresh DOM manipulation

--- advanced-javascript/library/script.js
+++ advanced-javascript/library/script.js
@@ -29,3 +29,14 @@ function addBookToLibrary(title, author, pages, isRead) {
   myLibrary.push(book);
   return;
 }
+
+addBookToLibrary("The Hobbit", "JRR Toliken", 295, false);
+addBookToLibrary("A Mind For Numbers", "Barbara Oakley", 450, true);
+
+const cardContainer = document.querySelector(".card-container");
+
+const card = document.createElement("div");
+card.className = "card";
+
+
+cardContainer.appendChild(card);
