94ccb87c728713d36e07f474265487f217087c79
haristotle <haristotlepatil2003@gmail.com>
2025-07-27 21:00:25 +0530

    Implement and test recursive levelOrderForEach()

--- data-structures-projects/balancedBST.js
+++ data-structures-projects/balancedBST.js
@@ -118,6 +118,38 @@ class Tree {
 
     return 0;
   }
+
+  // recursive
+  levelOrderForEach(callback, queue = null) {
+    try {
+      if (!callback) throw new Error("No callback provided.");
+
+      if (!queue) {
+        queue = [];
+        this.root.level = 0;
+        queue.push(this.root);
+      }
+
+      if (queue.length === 0) return 0;
+
+      const node = queue.shift();
+      callback(node);
+
+      if (node.leftChild) {
+        node.leftChild.level = node.level + 1;
+        queue.push(node.leftChild);
+      }
+      if (node.rightChild) {
+        node.rightChild.level = node.level + 1;
+        queue.push(node.rightChild);
+      }
+
+      return this.levelOrderForEach(callback, queue);
+    } catch (err) {
+      console.log(err);
+      return null;
+    }
+  }
 }
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
@@ -155,5 +187,6 @@ console.log(test.find(324));
 console.log(test.find(-1));
 
 test.levelOrderForEach((node) => {
-  console.log(node.data);
+  node.data *= 2;
 });
+prettyPrint(test.root);
