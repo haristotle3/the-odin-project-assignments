30ca391b737d4b591a424396f9cee87c60e33628
haristotle <haristotlepatil2003@gmail.com>
2025-07-27 19:57:59 +0530

    Implement and test deleteItem()

--- data-structures-projects/balancedBST.js
+++ data-structures-projects/balancedBST.js
@@ -11,15 +11,15 @@ class Tree {
     this.root = this.buildTree(array);
   }
 
-  buildTreeRecursive(array, left = 0, right = array.length - 1) {
+  #buildTreeRecursive(array, left = 0, right = array.length - 1) {
     const mid = Math.floor((left + right) / 2);
     if (mid < left) {
       return null;
     }
     const root = new Node(array[mid]);
 
-    root.leftChild = this.buildTreeRecursive(array, left, mid - 1);
-    root.rightChild = this.buildTreeRecursive(array, mid + 1, right);
+    root.leftChild = this.#buildTreeRecursive(array, left, mid - 1);
+    root.rightChild = this.#buildTreeRecursive(array, mid + 1, right);
 
     return root;
   }
@@ -27,16 +27,65 @@ class Tree {
   buildTree(array) {
     const arrWithoutDuplicates = [...new Set(array)];
     arrWithoutDuplicates.sort((x, y) => x - y);
-    return this.buildTreeRecursive(arrWithoutDuplicates);
+    return this.#buildTreeRecursive(arrWithoutDuplicates);
   }
 
-  insert(value, node = this.root) {
+  #insertRecursive(value, node = this.root) {
     if (node === null) return new Node(value);
     if (value === node.data) return node;
-    if (value < node.data) node.leftChild = this.insert(value, node.leftChild);
+
+    if (value < node.data)
+      node.leftChild = this.#insertRecursive(value, node.leftChild);
     if (value > node.data)
-      node.rightChild = this.insert(value, node.rightChild);
+      node.rightChild = this.#insertRecursive(value, node.rightChild);
     return node;
+  }
+
+  insert(value) {
+    this.root = this.#insertRecursive(value, this.root);
+    return 0;
+  }
+
+  #getInorderSuccessor(node) {
+    let successor = node.rightChild;
+    while (successor.leftChild) {
+      successor = successor.leftChild;
+    }
+
+    return successor;
+  }
+
+  #deleteItemRecursive(value, node = this.root) {
+    if (node === null) return node;
+
+    if (value < node.data)
+      node.leftChild = this.#deleteItemRecursive(value, node.leftChild);
+    else if (value > node.data)
+      node.rightChild = this.#deleteItemRecursive(value, node.rightChild);
+    else {
+      // no children
+      if (node.rightChild === null && node.leftChild === null) return null;
+      // only 1 children
+      if (node.rightChild === null) return node.leftChild;
+      else if (node.leftChild === null) return node.rightChild;
+      // if both children
+      else {
+        const inorderSuccessor = this.#getInorderSuccessor(node);
+
+        const temp = node.data;
+        node.data = inorderSuccessor.data;
+        inorderSuccessor.data = temp;
+
+        node.rightChild = this.#deleteItemRecursive(value, node.rightChild);
+      }
+    }
+
+    return node;
+  }
+
+  deleteItem(value) {
+    this.root = this.#deleteItemRecursive(value, this.root);
+    return 0;
   }
 }
 
