93169e4df17e585325b7ffdad486bcaff4292cab
haristotle <haristotlepatil2003@gmail.com>
2025-07-29 02:19:23 +0530

    Implement and test getMoves, getPath, breadthFirstSearch and knightMoves

--- data-structures-projects/knightTravails.js
+++ data-structures-projects/knightTravails.js
@@ -34,30 +34,81 @@ function neighbouringNodes(currentNode) {
   return neighbours;
 }
 
-// function breadthFirstSearch(start, end, queue) {
-//   const visited = new Array(8).fill(new Array(8).fill(false));
+function getMoves(currentNode, parentMatrix) {
+  let moves = 0;
+  let [wx, wy] = [currentNode[0], currentNode[1]];
 
-//   while (queue.length > 0) {
-//     const currentNode = queue.shift();
-//     const [x, y] = [currentNode[0], currentNode[1]];
+  while (parentMatrix[wx][wy][0] !== null && parentMatrix[wx][wy][1] !== null) {
+    [wx, wy] = parentMatrix[wx][wy];
+    moves++;
+  }
 
-//     if (!visited[x][y]) {
-//       if (arrayEqual(currentNode, end)) {
-//         return currentNode;
-//       }
+  return moves;
+}
 
-//       visited[x][y] = true;
+function getPath(currentNode, parentMatrix) {
+  const pathArr = [];
+  let [wx, wy] = [currentNode[0], currentNode[1]];
 
-//       for (let neighbour of neighbouringNodes(currentNode)) {
-//         queue.push(neighbour);
-//       }
-//     } else {
-//       return null;
-//     }
-//   }
-// }
+  while (parentMatrix[wx][wy][0] !== null && parentMatrix[wx][wy][1] !== null) {
+    pathArr.push([wx, wy]);
+    [wx, wy] = parentMatrix[wx][wy];
+  }
+  pathArr.push([wx, wy]);
+  return pathArr.reverse();
+}
+
+function breadthFirstSearch(end, queue) {
+  const visited = Array.from(Array(8), () => new Array(8).fill(false));
+  const enqueued = Array.from(Array(8), () => new Array(8).fill(false));
+  const parent = Array.from(Array(8), () => new Array(8).fill([null, null]));
+
+  while (queue.length > 0) {
+    const currentNode = queue.shift();
+
+    const [x, y] = [currentNode[0], currentNode[1]];
+    enqueued[x][y] = false;
+
+    if (!visited[x][y]) {
+      if (arrayEqual(currentNode, end)) {
+        return {
+          moves: getMoves(currentNode, parent),
+          path: getPath(currentNode, parent),
+        };
+      }
+
+      visited[x][y] = true;
+
+      for (let neighbour of neighbouringNodes(currentNode)) {
+        const [nx, ny] = [neighbour[0], neighbour[1]];
+        if (!visited[nx][ny] && !enqueued[nx][ny]) {
+          queue.push(neighbour);
+          enqueued[nx][ny] = true;
+          parent[nx][ny] = [x, y];
+        }
+      }
+    }
+  }
+  console.log(totalMoves);
+
+  return false;
+}
 
 function knightMoves(start, end) {
+  if (!isWithinBounds(end)) {
+    console.log("Unreachable!");
+    return null;
+  }
+
   const queue = [start];
-  return breadthFirstSearch(start, end, queue);
+  const pathObject = breadthFirstSearch(end, queue);
+
+  console.log(`You made it in ${pathObject.moves} moves! Here's your path:`);
+  pathObject.path.forEach((node) => {
+    console.log(node);
+  });
+  
+  return 0;
 }
+
+knightMoves([0, 0], [2, 2]);
