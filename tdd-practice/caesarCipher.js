e41e425b9cdc77f2fe2c1fe9cbf3d1be3439904a
haristotle <haristotlepatil2003@gmail.com>
2025-08-02 22:57:10 +0530

    Refactor caesarCipher.js

--- tdd-practice/caesarCipher.js
+++ tdd-practice/caesarCipher.js
@@ -1,6 +1,11 @@
+function getEncryptedChar(plaintextChar, offset, subtractor) {
+  const charCodeValue =
+    ((plaintextChar.charCodeAt(0) - subtractor + offset) % 26) + subtractor;
+  return String.fromCharCode(charCodeValue);
+}
+
 export default function caesarCipher(string, offset) {
   const A = "A".charCodeAt(0);
-  const Z = "Z".charCodeAt(0);
   const a = "a".charCodeAt(0);
   const z = "z".charCodeAt(0);
 
@@ -8,15 +13,8 @@ export default function caesarCipher(string, offset) {
     .split("")
     .map((char) => {
       if (char.charCodeAt(0) < A || char.charCodeAt(0) > z) return char;
-      if (char.charCodeAt(0) < a) {
-        return String.fromCharCode(
-          ((char.charCodeAt(0) - A + offset) % 26) + A
-        );
-      } else {
-        return String.fromCharCode(
-          ((char.charCodeAt(0) - a + offset) % 26) + a
-        );
-      }
+      if (char.charCodeAt(0) < a) return getEncryptedChar(char, offset, A);
+      else return getEncryptedChar(char, offset, a);
     })
     .join("");
 }
